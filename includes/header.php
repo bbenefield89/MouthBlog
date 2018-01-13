6deb214bdda0d0ee3442855726d76c7245853f10
Brandon Benefield <bsquared18@gmail.com>
2018-01-13 16:21:10 -0500

    Remove boilerplate & add dropdown for login form

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,32 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="assets/css/main.css">
 
     <title>Hello, world!</title>
   </head>
   <body>
+    
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="#">Mouth Blog</a>
+
+        <div class="dropdown">
+          <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Log in
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <form action="<?php htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, 'ISO-8859-15'); ?>" method="POST">
+              <div class="form-group">
+                <input class="form-control from-control-lg" type="text" name="email" placeholder="Email Address">
+              </div>
+              
+              <div class="form-group">
+                <input class="form-control from-control-lg" type="text" name="password" placeholder="Email Address">
+              </div>
+              <button class="btn btn-primary" type="submit" name="login_button">Log in</button>
+            </form>
+          </div>
+        </div>
+    </nav>
