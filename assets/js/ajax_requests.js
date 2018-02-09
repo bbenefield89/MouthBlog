a446891c4b2ce8247ecc0a3c27773b21c8885705
Brandon Benefield <bsquared18@gmail.com>
2018-02-08 21:23:31 -0500

    huge commit dump, woops

---
+++ assets/js/ajax_requests.js
@@ -0,0 +1,206 @@
+define(() => {
+  class AJAXRequests {
+    constructor() {
+      this.xhr = new XMLHttpRequest();
+    }
+    
+    // FIGURE THIS OUT
+    xhrMethods(method, url, sendData = {} ) {}
+    
+    // RETURN ALL POSTS
+    getAllPosts() {
+      return new Promise((resolve, reject) => {
+        this.xhr.open('GET', '//localhost/mouthblog/api/blog.php', true);
+        this.xhr.send();
+        
+        this.xhr.onload = () => {
+          if (this.xhr.status == 200) {
+            resolve(JSON.parse(this.xhr.response));
+          } else {
+            reject(this.xhr.statusText);
+          }
+        };
+        
+        this.xhr.onerror = () => {
+          reject(this.xhr.statusText);
+        };
+      });
+    }
+    
+    // DELETE POST
+    deletePostPromise(url, postID) {
+      return new Promise((resolve, reject) => {
+        this.xhr.open('POST', url, true);
+        this.xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        this.xhr.send(postID);
+        
+        this.xhr.onload = () => {
+          if (this.xhr.status == 200) {
+            resolve();
+          } else {
+            reject(this.xhr.statusText);
+          }
+        };
+        
+        this.xhr.onerror = () => {
+          reject(this.xhr.statusText);
+        };
+      });
+    }
+    
+    // SUBMIT NEW POST
+    submitPost(url, formData) {
+      return new Promise((resolve, reject) => {
+        this.xhr.open('POST', url, true);
+        
+        this.xhr.onload = () => {
+          if (this.xhr.status == 200) {
+            resolve();
+          } else {
+            reject(this.xhr.statusText);
+          }
+        };
+        
+        this.xhr.onerror = () => {
+          reject(this.xhr.statusText);
+        };
+        
+        this.xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        this.xhr.send(formData);
+      });
+    }
+    
+    // RETURN NEWEST POST
+    returnNewestPost(url) {
+      return new Promise((resolve, reject) => {
+        // const xhr = new XMLHttpRequest();
+        
+        this.xhr.open('GET', url, true);
+        
+        this.xhr.onload = () => {
+          if (this.xhr.status == 200) {
+            resolve(JSON.parse(this.xhr.response));
+          } else {
+            reject(this.xhr.statusText);
+          }
+        };
+        
+        this.xhr.onerror = () => {
+          reject(this.xhr.statusText);
+        };
+        
+        this.xhr.send();
+      });
+    }
+    
+    // HEART A POST
+    heartPost(url, formData) {
+      return new Promise((resolve, reject) => {
+        this.xhr.open('POST', url, true);
+        this.xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        this.xhr.send(formData);
+        
+        this.xhr.onload = () => {
+          if (this.xhr.status == 200) {
+            resolve();
+          } else {
+            reject(this.xhr.statusText);
+          }
+        };
+        
+        this.xhr.onerror = () => {
+          reject(this.xhr.statusText);
+        };
+      });
+    }
+    
+    // UNHEART A POST
+    unHeartPost(url, formData) {
+      return new Promise((resolve, reject) => {
+        this.xhr.open('POST', url, true);
+        this.xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        this.xhr.send(formData);
+        
+        this.xhr.onload = () => {
+          if (this.xhr.status == 200) {
+            resolve();
+          } else {
+            reject(this.xhr.statusText);
+          }
+        };
+        
+        this.xhr.onerror = () => {
+          reject(this.xhr.statusText);
+        };
+      });
+    }
+    
+    submitComment(url, formData) {
+      return new Promise((resolve, reject) => {
+        this.xhr.open('POST', url, true);
+        this.xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        this.xhr.send(formData);
+        
+        this.xhr.onload = () => {
+          if (this.xhr.status == 200) {
+            resolve();
+          } else {
+              reject(this.xhr.statusText);
+          }
+        };
+        
+        this.xhr.onerror = () => {
+          reject(this.xhr.statusText);
+        };
+      });
+    }
+    
+    returnNewestComment(url) {
+      return new Promise((resolve, reject) => {
+        this.xhr.open('GET', url, true);
+        this.xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        this.xhr.send();
+        
+        this.xhr.onload = () => {
+          if (this.xhr.status == 200) {
+            resolve(JSON.parse(this.xhr.response));
+          } else {
+            reject(this.xhr.statusText);
+          }
+        };
+        
+        this.xhr.onerror = () => {
+          reject(this.xhr.statusText);
+        };
+      });
+    }
+    
+    returnCurrentPostComments(data) {
+      return new Promise((resolve, reject) => {
+        this.xhr.open('POST', '//localhost/mouthblog/api/comments.php', true);
+        this.xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        this.xhr.send(data);
+        
+        console.log(data);
+        
+        this.xhr.onload = () => {
+          if (this.xhr.status == 200) {
+            console.log('SENDING');
+            resolve(JSON.parse(this.xhr.response));
+          } else {
+            console.log('REJECTING');
+            reject(this.xhr.statusText);
+          }
+        };
+        
+        this.xhr.onerror = () => {
+          console.log('ERROR');
+          reject(this.xhr.statusText);
+        };
+      });
+    }
+  }
+  
+  // RETURNING `AJAXRequests` CLASS AS `AJAXRequest`
+  return { AJAXRequests: AJAXRequests };
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,168 +1,292 @@
-// DELETE POST VARIABLES
-let deletePostBtn      = document.querySelectorAll('button[name="delete_post"]');
-const displayPostWrapper = document.querySelector('.col-8.pt-4');
-const displayPostSection = document.querySelectorAll('.col-8.pt-4 .row');
-const postID             = document.querySelectorAll('#delete-post-id');
-
-// SUBMIT POST VARIABLES
-const submitPostBtn = document.querySelector('#submit-post-button');
-const submitPostID  = document.querySelector('#submit-post-id');
-const submitPostContent  = document.querySelector('#submit-post-content');
-const submitPostName  = document.querySelector('#submit-post-name');
-
-// GET REQUEST TO RETRIEVE EVERY POST
-const get = (url) => {
-  return new Promise((resolve, reject) => {
-    const xhttp = new XMLHttpRequest();
-    
-    xhttp.open('GET', url, true);
-    
-    xhttp.onload = () => {
-      if (xhttp.status == 200) {
-        resolve(JSON.parse(xhttp.response));
-      } else {
-        reject(xhttp.statusText);
-      }
-    };
-    
-    xhttp.onerror = () => {
-      reject(xhttp.statusText);
-    };
-    
-    xhttp.send();
-  });
-}
-
-// DELETE SPECIFIC POST
-const deletePostPromise = (url, postID) => {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    
-    xhr.open('POST', url, true);
-    
-    xhr.onload = () => {
-      if (xhr.status == 200) {
-        console.log('if (xhr.status == 200)');
-        resolve();
-      } else {
-        reject(xhr.statusText);
-      }
-    };
-    
-    xhr.onerror = () => {
-      reject(xhr.statusText);
-    };
-    
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhr.send(postID);
-  });
-}
-
-// SUBMIT A NEW POST
-const submitPost = (url, user_id, user_name, content) => {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    
-    xhr.open('POST', url, true);
-    
-    xhr.onload = () => {
-      if (xhr.status == 200) {
-        console.log('resolving');
-        resolve();
-      } else {
-        reject(xhr.statusText);
-      }
-    };
-    
-    xhr.onerror = () => {
-      reject(xhr.statusText);
-    };
-    
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhr.send(user_id, user_name, content);
-  });
-};
-
-// RETURN THE NEWEST BLOG POST
-const returnNewestPost = (url) => {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    
-    xhr.open('GET', url, true);
-    
-    xhr.onload = () => {
-      if (xhr.status == 200) {
-        console.log('resolving');
-        resolve(JSON.parse(xhr.response));
-      } else {
-        reject(xhr.statusText);
-      }
-    };
-    
-    xhr.onerror = () => {
-      reject(xhr.statusText);
-    };
-    
-    xhr.send();
-  });
-}
-
-// MAKING THE CALL TO DELETE THE POST
-const delPostFunc = () => {
-  console.log(deletePostBtn);
-  deletePostBtn = document.querySelectorAll('button[name="delete_post"]');
-  console.log(deletePostBtn);
+require([ 'ajax_requests' ], function (aR) {
+  /**************
+  ** VARIABLES **
+  **************/
+  const ajaxRequests = new aR.AJAXRequests;
   
-  if (deletePostBtn) {  
-    for (let i = 0; i < deletePostBtn.length; i++) {
-      deletePostBtn[i].addEventListener('click', e => {
-        e.preventDefault();
-        console.log(deletePostBtn);
-        
-        displayPostSection[i].remove();
-        
-        // ${postID[i]} comes from `const postID` at the top
-        deletePostPromise('http://localhost/mouthblog/ajax/delete_post.ajax.php', `id=${postID[i].value}`);
-      });
-    }
-  }
-}
-// CALL `delPostFunc()` FOR THE INITIAL `deletePostBtn` ON SCREEN
-delPostFunc();
+  const submitPostBtn     = document.querySelector('#submit-post-button');
+  const submitPostID      = document.querySelector('#submit-post-id');
+  const submitPostContent = document.querySelector('#submit-post-content');
+  const submitPostName    = document.querySelector('#submit-post-name');
 
-// MAKING CALL TO SUBMIT NEW POST
-if (submitPostBtn) {
-  submitPostBtn.addEventListener('click', e => {
-    e.preventDefault();
-    
-    // SUBMIT POST
-    submitPost('http://localhost/mouthblog/ajax/submit_post.ajax.php',
-      `id=${submitPostID.value}&name=${submitPostName.value}&content=${submitPostContent.value}`);
-    
-    // RETURN THAT SAME POST
-    returnNewestPost('http://localhost/mouthblog/api/newest_post.php');
-    
-    // INSERT POST INTO DOM
-    const newPostDiv = document.createElement('div');
-    newPostDiv.setAttribute('class', 'row');
-    newPostDiv.innerHTML = `
-                            <article class="col-10 offset-1">
-                              <h2 class="h2">USERNAME</h2>
-                              <small>DATE CREATED</small>
-                              &nbsp;
-                              &nbsp;
-                              <form action="/mouthblog/blog.php" method="POST">
-                                <button class="btn btn-danger" name="delete_post" type="submit">DELETE</button>
-                                <input id="delete-post-id" name="post_id" type="hidden" value="USER ID">
-                              </form>
-                              <hr>
-                              <p class="lead">CONTENT</p>
-                            </article>
-    `;
-    
-    displayPostWrapper.prepend(newPostDiv);
-    
-    // GIVE THE `newPostDiv`'s `delete button` THE CLICK EVENT HANDLER
-    delPostFunc(); // BOOM!
+  const heartIconBtn = document.querySelectorAll('.heart-post-button');
+
+  const displayPostWrapper = document.querySelector('.col-8.pt-4');
+  
+  const submitCommentBtn         = document.querySelector('button[name="submit_comment"]');
+  const postModalCommentsWrapper = document.querySelector('.post-modal-comments-wrapper');
+  const newComment               = document.createElement('div');
+  
+  newComment.setAttribute('class', 'post-modal-comment mb-4');
+  
+  /******************************************************
+  ** PLACE `liked` CLASS ON HEARTED POSTS ON PAGE LOAD **
+  *******************************************************/
+  window.addEventListener('load', () => {
+    for (let i = 0; i < heartIconBtn.length; i++) {
+      if (heartIconBtn[i].classList[1] === 'liked') {
+        let heartIconSVG = heartIconBtn[i].children[0].children[0];
+        
+        heartIconBtn[i].children[0].children[0].classList.add('liked');
+      }
+    }
   });
-}
+
+  /*******************************
+  ** INERACTION WITH BLOG POSTS **
+  *******************************/
+  if (displayPostWrapper && submitPostBtn) {
+    const body                = document.querySelector('body');
+    const postModal           = document.querySelector('.post-modal-outer');
+    const postModalClose      = document.querySelector('.post-modal-close');
+    const postModalHeartCount = document.querySelector('#post-modal-heart-count');
+    const heartIcon           = document.querySelector('.fa-heart');
+      
+    // EVENT DELEGATION TO HANDLE ON CLICK EVENTS
+    displayPostWrapper.addEventListener('click', e => {
+      // console.log(e.path[1].classList);
+      
+      // POST MODAL FUNC
+      const postModalFunc = (childrenPath, postIDPath, heartCountPath) => {
+        const { children }        = e.path[childrenPath];
+        const postID              = e.path[postIDPath].children[5].value;
+        const userName            = children[0].innerText;
+        const dateCreated         = children[1].innerText;
+        const content             = children[4].innerText || children[3].innerText;
+        const deletePostButton    = children[2]['0'];
+        const postModalUsername   = document.querySelector('#post-modal-username');
+        const postCreatedOn       = document.querySelector('#post-modal-created-on');
+        const postModalContent    = document.querySelector('#post-modal-content');
+        const postModalPostID     = document.querySelector('.post-modal-post-id');
+        
+        // PREVENT SCROLLING
+        body.style.height   = '100%';
+        body.style.overflow = 'hidden';
+        
+        // ADD `postModal` CSS
+        postModal.style.display        = 'flex';
+        postModal.style.alignItems     = 'center';
+        postModal.style.justifyContent = 'center';
+        
+        // DYNAMICALLY INSERT CONTENT
+        postModalUsername.innerText = userName;
+        postCreatedOn.innerText     = dateCreated;
+        postModalContent.innerText  = content;
+        postModalPostID.value       = postID;
+      };
+      
+      // UNHEART POST
+      const unHeartPost = (icon, formPath, idPath, userPath, url = '//localhost/mouthblog/ajax/unheart_post.ajax.php') => {
+        e.preventDefault();
+        
+        const heartIcon = e.path[icon];
+        heartCount      = e.path[formPath].children[3];
+        heartCountNum   = Number(heartCount.innerText);
+        const post_id   = e.path[formPath].children[idPath];
+        const user_id   = e.path[formPath].children[userPath];
+        
+        const data = {
+          post_id: post_id.value,
+          user_id: user_id.value,
+        };
+        
+        heartCountNum--;
+        
+        heartCount.innerText = heartCountNum;
+        heartIcon.classList.remove('liked');
+        
+        ajaxRequests.unHeartPost(url, `post_id=${ data.post_id }&user_id=${ data.user_id }`);
+      };
+      
+      // HEARTFUNC
+      const heartFunc = (icon, formPath, url = '//localhost/mouthblog/ajax/heart_post.ajax.php') => {
+        e.preventDefault();
+        const heartIcon = e.path[icon];
+        let heartCount = e.path[formPath].children[3];
+        let heartCountNum = Number(heartCount.innerText);
+        const post_id = e.path[formPath].children[5];
+        const user_id = e.path[formPath].children[6];
+        const data = {
+          post_id: post_id.value,
+          user_id: user_id.value,
+        };
+        
+        heartCountNum++;
+        heartCount.innerText = heartCountNum;
+        heartIcon.classList.add('liked');
+        
+        ajaxRequests.heartPost(url, `post_id=${data.post_id}&user_id=${data.user_id}`);
+      };
+      
+      // RETURN ALL COMMENTS FOR PARTICULAR POST
+      const returnAllComments = (postIDPath) => {
+        const postID = e.path[postIDPath].children[5].value;
+        
+        ajaxRequests.returnCurrentPostComments(`post_id=${ postID }`)
+          .then(data => {
+            console.log(data);
+            
+            newComment.innerHTML = data.map(d => {
+                return `<div class="post-modal-comment-header mb-2">
+                  <p class="mb-0">${ d.username }</p>
+                  <small>${ d.date_created }</small>
+                </div>
+                <div class="post-modal-comment-content">
+                  <p>${ d.comment_content }</p>
+                </div>` ;
+              }).join('');
+              
+            postModalCommentsWrapper.prepend(newComment);
+          });
+      }
+      
+      // CHECK IF DELETE BUTTON WAS CLICKED
+      if (e.target.className === 'btn btn-danger') {
+        e.preventDefault();
+        
+        const { parentElement }        = e.target;
+        const row                      = parentElement.parentElement.parentElement;
+        const form                     = parentElement;
+        const postID                   = form.children[1].value;
+        const userID                   = form.children[2].value;
+        
+        row.style.opacity = '0';
+        
+        ajaxRequests.deletePostPromise('http://localhost/mouthblog/ajax/delete_post.ajax.php', `id=${postID}`);
+        ajaxRequests.unHeartPost('//localhost/mouthblog/ajax/unheart_post.ajax.php', `post_id=${ postID }&user_id=${ userID }`);
+        
+        setTimeout(() => {
+          row.remove();
+        }, 1000);
+        
+      // CHECK IF COMMENT ICON WAS CLICKED
+      } else if (e.path[0].className.baseVal === 'svg-inline--fa fa-comment-alt fa-w-18 fa-lg') {
+        returnAllComments(2);
+        postModalFunc(3, 2, 2);
+        
+      // SECOND CHECK IF COMMENT ICON WAS CLICKED
+      } else if (e.path[1].className.baseVal === 'svg-inline--fa fa-comment-alt fa-w-18 fa-lg') {
+        returnAllComments(3);
+        postModalFunc(4, 3, 3);
+        
+      // IF ALREADY `hearted` THEN REMOVE IT
+      } else if (e.path[1].classList[4] === 'liked') {
+        e.preventDefault();
+        unHeartPost(1, 4, 5, 6);
+        
+      // IF ALREADY `hearted` THEN REMOVE IT
+      } else if (e.path[0].className.baseVal === 'svg-inline--fa fa-heart fa-w-18 fa-lg liked') {
+        e.preventDefault();
+        unHeartPost(0, 3, 5, 6);
+        
+      // !!DONT DO ANYTHING!!
+      } else if (e.path[0].className === 'heart-post-button') {
+        e.preventDefault();
+      
+      // CHECK IF HEART ICON WAS CLICKED FROM `path`
+      } else if (e.path[0].parentElement.classList[1] === 'fa-heart') {
+        e.preventDefault();
+        console.log('CALLING heartFunc()');
+        heartFunc(1, 4);
+
+      // CHECK IF HEART ICON WAS CLICKED FROM `svg`
+      } else if (e.path[0].classList[1] === 'fa-heart') {
+        e.preventDefault();
+        console.log('CALLING heartFunc()');
+        heartFunc(0, 3);
+      }
+    }); // click event
+    
+    // CLOSING THE POST MODAL
+    postModal.addEventListener('click', e => {
+      // CHECK IF OUTER PORTON OF MODAL IS CLICKED OR `path` OR `svg` FOR THE `x` ICON ARE CLICKED
+      if (e.path[0].classList[0] === 'post-modal-outer' || e.path[2].classList[0] === 'post-modal-close' || e.path[1].classList[0] === 'post-modal-close') {
+        // ENABLE SCROLLING
+        body.style.height   = 'auto';
+        body.style.overflow = 'auto';
+        
+        // HIDE `postModal`
+        postModal.style.display = 'none';
+      }
+    });
+    
+    // MAKING CALL TO SUBMIT NEW POST
+    submitPostBtn.addEventListener('click', e => {
+      e.preventDefault();
+      
+      ajaxRequests.submitPost('http://localhost/mouthblog/ajax/submit_post.ajax.php',
+        `id=${submitPostID.value}&name=${submitPostName.value}&content=${submitPostContent.value}`)
+      .then(() => {
+        ajaxRequests.returnNewestPost('http://localhost/mouthblog/api/newest_post.php')
+          .then(data => {
+            const newPost = document.createElement('div');
+            
+            newPost.setAttribute('class', 'row post-container');
+            newPost.innerHTML = `
+                                <article class="col-10 offset-1" data-toggle="modal" data-target="#exampleModal">
+                                  <h2 class="h2">${ data.user_name }</h2>
+                                  <small>${ data.date_created }</small>
+                                  
+                                  <form action="//localhost/mouthblog/blog.php" method="POST">
+                                    <button class="btn btn-danger" name="delete_post" type="submit">DELETE</button>
+                                    <input id="delete-post-id" name="post_id" type="hidden" value="${ data.id }">
+                                    <input id="delete-post-user-id" name="user_id" type="hidden" value="${ data.user_id }">
+                                  </form>
+                                  
+                                  <hr>
+                                  <p class="lead">${ data.content }</p>
+                                  <hr>
+                                  
+                                  <form class="blog-post-interactions">
+                                    <i class="far fa-comment-alt fa-lg" data-toggle="modal" data-target="#exampleModal"></i>
+                                    <small class="mr-3">${ data.comment_count }</small>
+                                    <button class="heart-post-button" type="submit" name="heart_post_button"><i class="fas fa-heart fa-lg"></i></button>
+                                    <small>${ data.heart_count }</small>
+                                    <input name="heart_count" type="hidden" value="${ data.heart_count }">
+                                    <input name="post_id" type="hidden" value="${ data.id }">
+                                    <input name="user_id" type="hidden" value="${ data.user_id }">
+                                  </form>
+                                  
+                                </article>
+            `;
+            
+            displayPostWrapper.prepend(newPost);
+          }); // then
+      }); // then
+    }); // click event
+    
+    submitCommentBtn.addEventListener('click', e => {
+      e.preventDefault();
+      
+      const postID          = document.querySelector('.post-modal-post-id').value;
+      const commentUserID   = document.querySelector('.post-modal-comment-user-id').value;
+      const commentUsername = document.querySelector('.post-modal-comment-username').value;
+      const commentContent  = document.querySelector('.post-modal-comment-content').value;
+      
+      ajaxRequests.submitComment('//localhost/mouthblog/ajax/submit_comment.ajax.php',
+        `post_id=${ postID }&user_id=${ commentUserID }&username=${ commentUsername }&comment_content=${ commentContent }`)
+        .then(() => {
+          ajaxRequests.returnNewestComment('//localhost/mouthblog/api/newest_comment.php')
+            .then(data => {
+              // const postModalCommentsWrapper = document.querySelector('.post-modal-comments-wrapper');
+              // const newComment = document.createElement('div');
+              console.log(data);
+              
+              newComment.innerHTML = `
+                                      <div class="post-modal-comment-header mb-2">
+                                        <p class="mb-0">${ data.username }</p>
+                                        <small>${ data.date_created }</small>
+                                      </div>
+                                      <div class="post-modal-comment-content">
+                                        <p>${ data.comment_content }</p>
+                                      </div>
+              `;
+              
+              postModalCommentsWrapper.prepend(newComment);
+            });
+        });
+    });
+    
+    
+  } // if
+});
