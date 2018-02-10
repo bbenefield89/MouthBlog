23249a75867c4328fd4523388e6117375880a034
Brandon Benefield <bsquared18@gmail.com>
2018-02-10 02:01:20 -0500

    format code

--- includes/db/delete/delete_post.query.php
+++ includes/db/delete/delete_post.query.php
@@ -4,9 +4,11 @@ class DeletePost extends Connection {
   public function __construct($id) {
     $this->connect();
     
-    $sql = "DELETE FROM `posts`
-            WHERE `id`=:id";
-    $query = $this->connect()->prepare($sql);
+    $sql    = "DELETE FROM
+                 `posts`
+               WHERE
+                 `id` = :id";
+    $query  = $this->connect()->prepare($sql);
     $result = $query->execute(
       [
         ':id' => htmlentities($id, ENT_QUOTES, 'ISO-8859-15'),
